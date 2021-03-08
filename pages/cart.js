6e2c5d433d698c254a64212fc2187c509d1adfa1
Kruaks17 <60692360+Kruaks17@users.noreply.github.com>
2021-03-08 14:54:18 +0100

    commit

--- pages/cart.js
+++ pages/cart.js
@@ -1,39 +1,8 @@
 import React from 'react';
 
 
-export default function Cart ({cart,setCart}){
-
-    const getTotalSum = ()=>{
-        return cart.reduce(
-            (sum, {cost, quantity}) => sum + cost * quantity,
-            0
-        );
-    }
-
-const clearCart = () => {
-    setCart([]);
-};
-
-const setQuantity = (product, amount) => {
-
-    const newCart = [...cart];
-    newCart.find(
-        (item) => item.navn === product.id
-    ).quantity = amount;
-    setCart(newCart);
-};
-
-const removeFromCart = (productsToRemove) => {
-    setCart (
-        cart.filter((products)=> products !== productsToRemove)
-    );
-};
-
-return (
-    <>
-
-    </>
-
-
-)
-};
+Cart.getInitialProps= async function(){
+    const res = await fetch(
+        ''
+    )
+}
